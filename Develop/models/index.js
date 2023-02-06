2c39028026f28096eff8caf15f6d62f1024f5377
osjarju <115750838+osjarju@users.noreply.github.com>
2023-02-06 13:44:39 -0800

    index.js filled

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,10 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
 
